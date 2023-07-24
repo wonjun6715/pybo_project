aa73906e32a74aecd2f3c286f45900d956d9725b
wonjun Lee <wj6715@naver.com>
2023-07-24 20:44:59 +0900

    pybo 내용 출력

--- pybo/views.py
+++ pybo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
 
 def index(request):
@@ -9,3 +9,11 @@ def index(request):
     context = {'question_list': question_list} # render 함수가 템플릿을 HTML로 변환하는 과정에서 사용되는 데이터 변수 context
     return render(request, 'pybo/question_list.html', context)
     # render 함수는 context에 있는 question_list를 pybo/question_list.html 파일에 적용하여 HTML 코드로 변환
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
