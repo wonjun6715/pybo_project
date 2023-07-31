753f9da80d7883fadcd47ca6eeff451951e07152
wonjun Lee <wj6715@naver.com>
2023-07-31 20:49:46 +0900

    add paging

--- pybo/views.py
+++ pybo/views.py
@@ -2,13 +2,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 def index(request):
     """
     pybo 목록 출력
     """
+    # 입력 인자
+    page = request.GET.get('page', '1') # 페이지, 맨처음 불러오는 페이지가 1
+
+    # 조회
     question_list = Question.objects.order_by('-create_date') # 데이터를 작성한 날짜의 역순(-)으로 조회하기 위해 order-by 함수 사용
-    context = {'question_list': question_list} # render 함수가 템플릿을 HTML로 변환하는 과정에서 사용되는 데이터 변수 context
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj} # render 함수가 템플릿을 HTML로 변환하는 과정에서 사용되는 데이터 변수 context
     return render(request, 'pybo/question_list.html', context)
     # render 함수는 context에 있는 question_list를 pybo/question_list.html 파일에 적용하여 HTML 코드로 변환
 
