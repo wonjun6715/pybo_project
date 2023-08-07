d2da1ef88140589a2b095d338fec04b6bcbf449b
wonjun Lee <wj6715@naver.com>
2023-08-07 20:54:16 +0900

    질문 삭제 기능 추가

--- pybo/models.py
+++ pybo/models.py
@@ -6,6 +6,7 @@ class Question(models.Model):
     subject = models.CharField(max_length=200) # 글자 수를 제한하고 싶을 때는 CharField를 사용
     content = models.TextField() # 글자 수 제한이 없는 데이터
     create_date = models.DateTimeField() # 날짜, 시간 관련 속성은 DateTimeField 사용
+    modify_date = models.DateTimeField(null=True, blank=True) # 질문을 언제 수정했는지 확인, 질문 수정일시
 
 
     def __str__(self):
@@ -17,3 +18,5 @@ class Answer(models.Model):
     # 모델을 속성으로 가지면 ForeignKey 이용, CASCADE는 종속이라는 뜻, 질문이 삭제되면 답변도 삭제 되어야 함
     content = models.TextField()
     create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True) # blank=True의 의미는 form.is_vaild()를 통한 입력 폼 데이터 검사시 값이 없어도 된다는 의미
+                                                              # 답변 수정일시, 수정일시는 수정한 경우에만 생성되는 데이터이므로 저건 지정
--- pybo/views.py
+++ pybo/views.py
@@ -4,6 +4,7 @@ from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def index(request):
     """
@@ -74,3 +75,41 @@ def question_create(request):
         form = QuestionForm() # request 메세지가 'GET'인 경우 호출
     context = {'form' : form}
     return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    """
+    pybo 질문 수정
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author: # 로그인한 사용자와 수정하려는 글쓴이가 다르다면
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question) # instance 매개변수에 question을 지정하면 기존 값을 폼에 채울 수 있음
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user
+            question.modify_date = timezone.now() # 질문 수정일시를 현재일시로 저장
+            question.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    """
+    pybo 질문 삭제
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('pybo:detail', question_id=question.id)
+    question.delete()
+    return redirect('pybo:index')
+
+
+
