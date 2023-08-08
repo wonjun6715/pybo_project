e287ee68df214bc8d7e0945a18e7debcafdb7e78
wonjun Lee <wj6715@naver.com>
2023-08-08 17:17:15 +0900

    view file 분리

--- pybo/views/__init__.py
+++
@@ -1,4 +0,0 @@
-# views 디렉터리에 잇는 모든 뷰 파일 함수를 import 했으므로 pybo/urls.py와 같은 다른 모듈에서 views 모듈의 함수를 사용하는 부분을 수정할 필요 x
-from .base_views import *
-from .question_views import *
-from .answer_views import *
