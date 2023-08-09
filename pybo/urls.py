4c9d3954d3a68936883c230758026505bf007c9e
wonjun Lee <wj6715@naver.com>
2023-08-09 21:31:53 +0900

    추천 기능 완성

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import base_views, question_views, answer_views, comment_views
+from .views import base_views, question_views, answer_views, comment_views, vote_views
 
 app_name = 'pybo' # namespace 이름
 
@@ -18,8 +18,7 @@ urlpatterns = [
          question_views.question_modify, name='question_modify'),
     path('question/delete/<int:question_id>/',
          question_views.question_delete, name='question_delete'),
-    path('question/vote/<int:question_id>/',
-         question_views.question_vote, name='question_vote'),
+
 
     # answer_views.py
     path('answer/create/<int:question_id>/',
@@ -28,8 +27,7 @@ urlpatterns = [
          answer_views.answer_modify, name='answer_modify'),
     path('answer/delete/<int:answer_id>/',
          answer_views.answer_delete, name='answer_delete'),
-    path('answer/vote/<int:answer_id>/',
-         answer_views.answer_vote, name='answer_vote'),
+
 
     # comment_views.py
     path('comment/create/question/<int:question_id>/',
@@ -38,4 +36,10 @@ urlpatterns = [
          comment_views.comment_modify_question, name='comment_modify_question'),
     path('comment/delete/question/<int:comment_id>/',
          comment_views.comment_delete_question, name='comment_delete_question'),
+
+    # vote_views.py
+    path('vote/question/<int:question_id>/',
+         vote_views.vote_question, name='vote_question'),
+    path('vote/answer/<int:answer_id>/',
+         vote_views.vote_answer, name='vote_answer'),
 ]
