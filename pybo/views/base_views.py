04b07e85a6c31e4ce27acadb4bf1cbfca4e657e4
wonjun Lee <wj6715@naver.com>
2023-08-17 17:33:11 +0900

    검색 기능 추가

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 from ..models import Question
 
@@ -9,9 +10,18 @@ def index(request):
     """
     # 입력 인자
     page = request.GET.get('page', '1') # 페이지, 맨처음 불러오는 페이지가 1
+    kw = request.GET.get('kw', '') # 검색어
 
     # 조회
     question_list = Question.objects.order_by('-create_date') # 데이터를 작성한 날짜의 역순(-)으로 조회하기 위해 order-by 함수 사용
+    if kw:
+        # kw는 keyword를 의미, Q함수의 icontains는 kw가 각각의 내용에 포함되어있는지를 의미
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) | # 제목 검색
+            Q(content__icontains=kw) | # 내용 검색
+            Q(author__username__icontains=kw) | # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw) # 답변 글쓴이 검색
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10) # 페이지당 10개씩 보여주기
@@ -22,7 +32,7 @@ def index(request):
     # 페이징 객체 생성
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj, 'max_index' : max_index} # render 함수가 템플릿을 HTML로 변환하는 과정에서 사용되는 데이터 변수 context
+    context = {'question_list': page_obj, 'max_index': max_index, 'page': page, 'kw': kw} # render 함수가 템플릿을 HTML로 변환하는 과정에서 사용되는 데이터 변수 context
     return render(request, 'pybo/question_list.html', context)
     # render 함수는 context에 있는 question_list를 pybo/question_list.html 파일에 적용하여 HTML 코드로 변환
 
